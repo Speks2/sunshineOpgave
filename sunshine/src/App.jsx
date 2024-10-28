80360b82cc3d94e1083f77ebd13a2abdae7f8598
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-10-28 12:34:05 +0100

    we begun

---
+++ sunshine/src/App.jsx
@@ -0,0 +1,20 @@
+import { Top } from './Top/Top';
+import { Middle } from './Middle/Middle';
+import { Bottom } from './Bottom/Bottom';
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Top />
+     <Middle />
+     <Bottom />
+    </>
+  )
+}
+
+export default App
---
+++ sunshine/src/Middle/Middle.jsx
@@ -0,0 +1,1 @@
+import styles from './Middle.module.scss';
