a713c545bb44f40d07b1c6e2c067f5e1347a3006
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-10-31 12:36:21 +0100

    fixed some html and styling

--- sunshine/src/components/Footer/Footer.jsx
+++ sunshine/src/components/Footer/Footer.jsx
@@ -8,23 +8,30 @@ export function Footer() {
 
         <>
         <footer>
-            <div className={styles.Footer}>
-             <div>
-             <h2>Contact</h2>
-             <p>Supercoffeeroad 223b92230 New Coffeland Phone: 22331122 Mail: coffeeland@info.com</p>
-             </div>
-             <div>
-                <h2>Legal</h2>
-                <p>Cookie policy Retun policy Shipping Terms and Conditions</p>
-             </div>
-             <div>
-                <h2>About</h2>
-                <p>History The people behind Partnerships International</p>
-             </div>
+            <div className={styles.FooterText}>
+                <ul>
+                    <li><h3>Contact</h3></li>
+                    <li>Supercoffeeroad 223b</li>
+                    <li>92230 New Coffeland</li>
+                    <li>Phone: 22331122</li>
+                    <li>Mail: coffeeland@info.com</li>
+                </ul>
+                <ul>
+                    <li><h3>Legal</h3></li>
+                    <li>Cookie Policy</li>
+                    <li>Return policy</li>
+                    <li>Shipping</li>
+                    <li>Terms and Conditions</li>
+                </ul>
+                <ul>
+                    <li><h3>About</h3></li>
+                    <li>History</li>
+                    <li>The People behind</li>
+                    <li>Partnerships</li>
+                    <li>International</li>
+                </ul>
             </div>
-            
         </footer>
-        
         </>
         )
 } 
--- sunshine/src/components/Main/Main.jsx
+++ sunshine/src/components/Main/Main.jsx
@@ -8,22 +8,37 @@ export function Main() {
     return (
         <>
         <container className={styles.TextAndImages}>
-            <div>
+            <figure>
+            <figcaption>
                 <h2>The right beans</h2>
                 <p>Our journey begins with a deep-rooted connection to the land, as we seek out farmers who uphold environmentally-friendly practices and prioritize the well-being of their communities. Through personal relationships and mutual respect, we collaborate closely with these farmers, understanding their methods and values.</p>
+            </figcaption>
              <img src="src/assets/hands.png" alt="beans1" />
-            </div>
-            <div>
+            </figure>
+            <figure>
+            <figcaption>
                 <h2>Carefully handled</h2>
                 <p>Each bean is carefully handpicked at the peak of ripeness, ensuring optimal flavor and aroma. We embrace diversity in our selection, cherishing the unique characteristics of each region and varietal. From the lush mountainsides to the sun-kissed valleys, we traverse the landscapes in search of perfection.</p>
+            </figcaption>
              <img src="src/assets/sacks.png" alt="beans2" />
-            </div>
-            <div>
+            </figure>
+            <figure>
+            <figcaption>
                 <h2>From us to you</h2>
                 <p>Our commitment to organic farming means that our beans are free from harmful chemicals, allowing the natural flavors to shine through. We believe in transparency and traceability, providing our customers with a genuine connection to the origins of their coffee.</p>
+            </figcaption>
              <img src="src/assets/coffeeHeart.png" alt="beans3" />
-            </div>
+            </figure>
         </container>
+
+        <container className={styles.ShopNow}>
+        <article>
+           <h2>Shop now</h2>
+           <p>Our delicious coffees wil get you brewing the best cup of coffee you ever tasted in no time at all. And the best part of it? It is totally organic, fair trade and sustainably sourced. So get brewing</p>
+           <button>Go to products</button>
+        </article>
+        </container>
+
         </>
     )
 }
